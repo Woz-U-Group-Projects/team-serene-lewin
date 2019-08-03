6a0318fd8c9e1e4c711a4b89c0bd8ab2fe1baec2
fatpat77 <43324090+fatpat77@users.noreply.github.com>
2019-08-02 20:55:09 -0500

    created JWT

---
+++ backend/demo/src/main/models/Entree.java
@@ -0,0 +1,35 @@
+package com.example.demo.models;
+
+public class Entree {
+	
+	private int itemId;
+	private String itemName;
+	private String itemDesc;
+	private int itemPrice;
+	
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getItemDesc() {
+		return itemDesc;
+	}
+	public void setItemDesc(String itemDesc) {
+		this.itemDesc = itemDesc;
+	}
+	public int getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+}
