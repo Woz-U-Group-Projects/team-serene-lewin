5d6f0877f19bb0ae1cf3aef31af97326ef80a4bb
fatpat77 <43324090+fatpat77@users.noreply.github.com>
2019-08-11 20:17:25 -0500

    added closing brace to entree controler and added @repository to entree repository

---
+++ frontend/entree.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const item1number = "E1"
+const item1 = "MOROCCAN CHICKEN"
+const item2number = ""
+const item2 =
+const item3number =
+const item3 =
+const item4number
+const item4
+const item5number
+const item5  
+const item6number
+const item6
+const item7number
+const item7
+const item8number
+const item8
+const item9number
+const item9
+const item10number
+const item10 
+const item11number
+const item11
+const item12number
+const item12
+const item13number
+const item13
+const item14number
+const item14
+const item15number
+const item15  
+const item16number
+const item16
+const item17number
+const item17
+const item18number
+const item18
+const item19number
+const item19
+const item20number
+constitem20
+
+ReactDOM.render(
+    
+    document.getElementById('entree')
+  );
