b908cfb53b1ef873432d3add597d6312de2cc501
Carolyn <slitscreen@outlook.com>
2019-08-15 11:39:18 -1000

    updated controllers with @autowire annotation; code cleanup

--- backend/MenuMaestro/src/main/java/com/example/demo/controllers/DessertController.java
+++ backend/MenuMaestro/src/main/java/com/example/demo/controllers/DessertController.java
@@ -6,58 +6,57 @@ import java.util.List;
 import com.example.demo.models.Dessert;
 import com.example.demo.repositories.DessertRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
-
 public class DessertController {
 
+	@Autowired 
 	private DessertRepository dessertRepository;
 
-    public DessertController(DessertRepository dessertRepository) {
-        this.dessertRepository = dessertRepository;
-    }
-	
-	@GetMapping("/dessert")
+	@GetMapping("/desserts")
 	public List<Dessert> getDesserts(Dessert dessert) {
+		
 		List <Dessert> desserts = new ArrayList<Dessert>();
 		
-		 dessertRepository.findAll();
+		dessertRepository.findAll();
 		
-		 return desserts;
-      }
+		return desserts;
+	}
 	
-	 @GetMapping("/dessert/{id}")
-	    public ResponseEntity<Dessert> getDessert(@PathVariable(value="id") String id) {
-	        Dessert foundDessert = dessertRepository.findById(id).orElse(null);
-
-	        if(foundDessert == null) {
-	            return ResponseEntity.notFound().header("Dessert","Nothing found with that id").build();
-	        }
-	        return ResponseEntity.ok(foundDessert);
-	    }
-	
-
-    @PostMapping("/dessert")
-    public ResponseEntity<Dessert> postMessage(@RequestBody Dessert dessert) {
-
-       
-        Dessert createdDessert = dessertRepository.save(dessert);
-
-        return ResponseEntity.ok(createdDessert);
-    }
-    
-    @DeleteMapping("/Dessert/{id}")
-    public ResponseEntity<Dessert> deleteDessert(@PathVariable(value="id") String id) {
-        Dessert foundDessert = dessertRepository.findById(id).orElse(null);
+	@GetMapping("/dessert/{itemid}")
+    public ResponseEntity<Dessert> getDessert(@PathVariable(value="itemId") String itemId) {
+        
+		Dessert foundDessert = dessertRepository.findById(itemId).orElse(null);
 
         if(foundDessert == null) {
             return ResponseEntity.notFound().header("Dessert","Nothing found with that id").build();
-        }else {
+        }
+        
+        return ResponseEntity.ok(foundDessert);
+    }
+
+    @PostMapping("/dessert")
+    public ResponseEntity<Dessert> postMessage(@RequestBody Dessert dessert) {
+       
+    	Dessert createdDessert = dessertRepository.save(dessert);
+
+    	return ResponseEntity.ok(createdDessert);	
+    }
+    
+    @DeleteMapping("/dessert/{id}")
+    public ResponseEntity<Dessert> deleteDessert(@PathVariable(value="id") String id) {
+        
+    	Dessert foundDessert = dessertRepository.findById(id).orElse(null);
+
+        if(foundDessert == null) {
+            return ResponseEntity.notFound().header("Dessert","Nothing found with that id").build();
+        } else {
         	dessertRepository.delete(foundDessert);
         }
+        
         return ResponseEntity.ok().build();
     }
 }
--- backend/MenuMaestro/src/main/java/com/example/demo/controllers/EmployeeController.java
+++ backend/MenuMaestro/src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -1,43 +1,89 @@
 package com.example.demo.controllers;
 
-import com.example.demo.models.Employee;
-import com.example.demo.repositories.EmployeeRepository;
-import org.springframework.web.bind.annotation.*;
-
+import java.util.ArrayList;
 import java.util.List;
 
+import com.example.demo.models.Employee;
+import com.example.demo.repositories.EmployeeRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
-@RequestMapping("/employees")
 public class EmployeeController {
     
+	@Autowired
 	private EmployeeRepository employeeRepository;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+	@GetMapping("/employees")      
+    public List<Employee> getEmployees(Employee employee) {
+		
+    	List <Employee> employees = new ArrayList<Employee>();
+		
+		employeeRepository.findAll();
+		
+		return employees;
     }
-
-    @GetMapping("/all")
-    public List<Employee> getAll(){
-        List<Employee> employees = this.employeeRepository.findAll();
+	
+    @GetMapping("/employee/{name}")
+    public ResponseEntity<Employee> getEmployeeByName(@PathVariable(value="name") String name) {
         
-        return employees;
+    	Employee foundEmployee = employeeRepository.findById(name).orElse(null);
+
+    	if(foundEmployee == null) {
+            return ResponseEntity.notFound().header("Employee","None found with that name").build();
+        }
+        
+        return ResponseEntity.ok(foundEmployee);
+    }
+	
+	@GetMapping("/employee/{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable(value="id") String id) {
+        
+    	Employee foundEmployee = employeeRepository.findById(id).orElse(null);
+
+    	if(foundEmployee == null) {
+            return ResponseEntity.notFound().header("Employee","Nothing found with that id").build();
+        }
+        
+        return ResponseEntity.ok(foundEmployee);
     }
 
-    @PutMapping
-    public void insert(@RequestBody Employee employee){
-        this.employeeRepository.insert(employee);
+    @PostMapping("/employee")
+    public ResponseEntity<Employee> postMessage(@RequestBody Employee employee) {
+       
+    	Employee createdEmployee = employeeRepository.save(employee);
+
+    	return ResponseEntity.ok(createdEmployee);	
     }
 
-    @PostMapping
-    public void update(@RequestBody Employee employee){
-        this.employeeRepository.save(employee);
+    @DeleteMapping("/employee/{name}")
+    public ResponseEntity<Employee> deleteEmployeeByName(@PathVariable(value="name") String name) {
+        
+    	Employee foundEmployee = employeeRepository.findByName(name);
+
+        if(foundEmployee == null) {
+            return ResponseEntity.notFound().header("Employee","None found with that name").build();
+        } else {
+        	employeeRepository.delete(foundEmployee);
+        }
+        
+        return ResponseEntity.ok().build();
     }
+    
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<Employee> deleteEmployeeById(@PathVariable(value="id") String id) {
+        
+    	Employee foundEmployee = employeeRepository.findById(id).orElse(null);
 
-    @GetMapping("/employee")
-    public Employee getById(@PathVariable("employeeName") String employeeName){
-        Employee employee = this.employeeRepository.findByName(employeeName);
-
-        return employee;
+        if(foundEmployee == null) {
+            return ResponseEntity.notFound().header("Employee","None found with that id").build();
+        } else {
+        	employeeRepository.delete(foundEmployee);
+        }
+        
+        return ResponseEntity.ok().build();
     }
 
 }
